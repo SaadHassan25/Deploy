30875a6185db8612c864af405aadd602bae04ddc
Muhammad Saad Hassan <itissaad25@gmail.com>
2025-09-07 17:05:46 +0500

    settings update

--- aiBlogs/settingprod.py
+++ aiBlogs/settingprod.py
@@ -72,6 +72,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from a .env file if present
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +84,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4e=*elgde7%-l(@)tyur!!f=^o#9z4laqpqfken*xg4579cj)('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -138,8 +141,13 @@ WSGI_APPLICATION = 'aiBlogs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Blogs',
+        'USER': 'Blogs',
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
@@ -196,14 +204,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # For development, we'll use console backend to print emails to console
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# In production, you would use SMTP settings like:
+# # In production, you would use SMTP settings like:
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 465
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+# EMAIL_HOST_USER = 'itissaad25@gmail.com'
+# EMAIL_HOST_PASSWORD = 'kgjcqwtmpdxbqkev'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'itissaad25@gmail.com'
-EMAIL_HOST_PASSWORD = 'kgjcqwtmpdxbqkev'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False').lower() == 'true'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
 
 # # Use same Gmail address as default sender
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
