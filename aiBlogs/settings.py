e154ab1e943cf3bdf9e95aee888bb5b7b9d27329
Muhammad Saad Hassan <itissaad25@gmail.com>
2025-09-14 19:06:37 +0500

    Hidding secret key

--- aiBlogs/settings.py
+++ aiBlogs/settings.py
@@ -83,7 +83,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4e=*elgde7%-l(@)tyur!!f=^o#9z4laqpqfken*xg4579cj)('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
